85779d8b6da47488b7c12ae21046304744178017
Denis_L <denis88lyapin@gmail.com>
2023-09-26 19:37:49 +0300

    Добавлен CRUD для модели Lesson

--- school/urls.py
+++ school/urls.py
@@ -1,7 +1,10 @@
+from django.urls import path
+
 from school.apps import SchoolConfig
 from rest_framework.routers import DefaultRouter
 
-from school.views import CourseViewSet
+from school.views import (CourseViewSet, LessonCreateAPIView, LessonListAPIView, LessonRetrieveAPIView,
+                          LessonUpdateAPIView, LessonDestroyAPIView)
 
 app_name = SchoolConfig.name
 
@@ -9,5 +12,9 @@ router = DefaultRouter()
 router.register(r'courses', CourseViewSet, basename='courses')
 
 urlpatterns = [
-
-              ] + router.urls
+    path('lesson/create/', LessonCreateAPIView.as_view(), name='lesson_create'),
+    path('lesson/', LessonListAPIView.as_view(), name='lesson_list'),
+    path('lesson/<int:pk>/detail/', LessonRetrieveAPIView.as_view(), name='lesson_detail'),
+    path('lesson/<int:pk>/update/', LessonUpdateAPIView.as_view(), name='lesson_update'),
+    path('lesson/<int:pk>/delete/', LessonDestroyAPIView.as_view(), name='lesson_delete'),
+] + router.urls
