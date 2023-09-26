751cdec76d3a6ab8104ff769735e0018b427232a
Denis_L <denis88lyapin@gmail.com>
2023-09-26 20:16:58 +0300

    Добавлен UserUpdateAPIView

--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('school.urls', namespace='courses'))
+    path('', include('school.urls', namespace='courses')),
+    path('user/', include('users.urls', namespace='users'))
 ]
