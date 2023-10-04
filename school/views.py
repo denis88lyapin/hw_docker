a51e43bd38081b0efbc1deb19bb9ac2e5cd09815
Denis_L <denis88lyapin@gmail.com>
2023-10-04 21:56:14 +0300

    Задания 2-3.

--- school/views.py
+++ school/views.py
@@ -18,6 +18,7 @@ class CourseViewSet(viewsets.ModelViewSet):
         else:
             return Course.objects.filter(owner=self.request.user)
 
+
 class LessonCreateAPIView(generics.CreateAPIView):
     serializer_class = LessonListSerializer
     permission_classes = [IsAuthenticated, ~IsModerator]
@@ -28,8 +29,13 @@ class LessonCreateAPIView(generics.CreateAPIView):
 
 class LessonListAPIView(generics.ListAPIView):
     serializer_class = LessonListSerializer
-    queryset = Lesson.objects.all()
     permission_classes = [IsAuthenticated, IsOwnerOrSuperuser | IsModerator]
+
+    def get_queryset(self):
+        if self.request.user.groups.filter(name='moderators').exists():
+            return Lesson.objects.all()
+        else:
+            return Lesson.objects.filter(owner=self.request.user)
 
 
 class LessonRetrieveAPIView(generics.RetrieveAPIView):
@@ -46,4 +52,4 @@ class LessonUpdateAPIView(generics.UpdateAPIView):
 
 class LessonDestroyAPIView(generics.DestroyAPIView):
     queryset = Lesson.objects.all()
-    permission_classes = [IsAuthenticated, IsOwnerOrSuperuser]
+    permission_classes = [IsAuthenticated, IsOwnerOrSuperuser, ~IsModerator]
--- users/views.py
+++ users/views.py
@@ -5,6 +5,10 @@ from users.models import User
 from users.serializers import UserSerializer
 
 
+
+
+
+
 class UserUpdateAPIView(generics.UpdateAPIView):
     serializer_class = UserSerializer
     queryset = User.objects.all()
