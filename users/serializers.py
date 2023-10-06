4ead0994a0b3cdf5f2179199af7d2b5105a23992
Denis_L <denis88lyapin@gmail.com>
2023-10-06 17:53:28 +0300

    Исправлен UserViewSet

--- users/serializers.py
+++ users/serializers.py
@@ -14,3 +14,19 @@ class UserPublicSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'email', 'phone', 'city', 'avatar', 'last_login')
+
+
+
+class UserCreateSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ('email', 'phone', 'city', 'avatar', 'password')
+        # extra_kwargs = {
+        #     'phone': {'required': False},
+        #     'city': {'required': False},
+        #     'avatar': {'required': False},
+        # }
+
+
+
--- users/views.py
+++ users/views.py
@@ -1,8 +1,10 @@
-from rest_framework.permissions import IsAuthenticated
-from rest_framework import viewsets
+import json
+
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework import viewsets, status
 
 from users.models import User
-from users.serializers import UserSerializer, UserPublicSerializer
+from users.serializers import UserSerializer, UserPublicSerializer, UserCreateSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -10,15 +12,41 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     permission_classes = [IsAuthenticated]
 
+    def get_permissions(self):
+        if self.action == 'create':
+            return [AllowAny()]
+        else:
+            return super().get_permissions()
+
     def get_serializer_class(self):
         if self.action == 'list':
             return UserPublicSerializer
-        elif (self.action in ['retrieve', 'update', 'partial_update'] and self.request.user == self.get_object() or
-              self.request.user.is_superuser):
+        elif (self.action in ['retrieve', 'update', 'partial_update', 'destroy'] and
+              self.request.user == self.get_object() or self.request.user.is_superuser):
             return UserSerializer
         elif self.action == 'retrieve' and self.request.user != self.get_object():
             return UserPublicSerializer
         elif self.action == 'create':
-            return UserSerializer
-        elif self.action == 'destroy' and self.request.user == self.get_object() or self.request.user.is_superuser:
-            return UserSerializer
+            return UserCreateSerializer
+
+
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+        if response.status_code == status.HTTP_201_CREATED:
+            instance = self.get_queryset().get(email=response.data['email'])
+            password = request.data.get('password')
+            if password:
+                instance.set_password(password)
+                instance.save()
+        return response
+
+    def update(self, request, *args, **kwargs):
+        response = super().update(request, *args, **kwargs)
+        instance = self.get_object()
+        new_password = request.data.get('password')
+        if new_password:
+            instance.set_password(new_password)
+            instance.save()
+        return response
+
+
